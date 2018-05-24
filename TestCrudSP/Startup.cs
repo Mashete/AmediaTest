99061eaa3cf413237a750b150bd876d8ce711021
santiagojprado@gmail.com <santiagojprado@gmail.com>
2018-05-23 21:10:34 -0300

    Agregar archivos de proyecto.

---
+++ TestCrudSP/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TestCrudSP.Startup))]
+namespace TestCrudSP
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
